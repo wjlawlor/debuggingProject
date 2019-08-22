eb694b4e4539b763b5303169a15394ec9a0436b5
wjlawlor <wjl28@msn.com>
2019-08-22 10:29:47 -0400

    Full debugging project.

---
+++ errorHandlingExample/Default.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Trace="true" Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="errorHandlingExample.Defult" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Error Handling Example</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:TextBox ID="Number1" runat="server" />
+        <asp:TextBox ID="Number2" runat="server" />
+
+        <asp:Button ID="Sum" runat="server" Text="Add" OnClick="Sum_Click" />
+
+        <asp:Label ID="Result" runat="server" /><%= Sum %>
+        
+        <br />
+        <asp:Hyperlink runat="server" NavigateUrl="~/Default.aspx" Text="Clear" Value="Home" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ errorHandlingExample/Default.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Diagnostics;
+
+namespace errorHandlingExample
+{
+    public partial class Defult : System.Web.UI.Page
+    {
+        protected void Sum_Click(object sender, EventArgs e)
+        {
+            Trace.Write("Button Clicked", "Button was Clicked");
+
+            int x;
+            int y;
+            int answer;
+            if (int.TryParse(Number1.Text, out x) == true && int.TryParse(Number2.Text, out y) == true)
+            {
+                answer = x + y;
+                Result.Text = answer.ToString();
+
+                Trace.Write("Result Written", $"{answer}");
+
+                System.Diagnostics.Debug.Assert(answer != 21);
+            }
+            else
+            {
+                Result.Text = "Error";
+
+                Trace.Warn("Button Clicked", "Could not parse both as numbers.");
+            }
+        }
+    }
+}
